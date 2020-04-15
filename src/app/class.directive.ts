8125042b5c78b8ba3e6ac44d7bc1583f69579025
sangTran <sangthien2010@gmail.com>
2020-04-14 21:43:54 -0500

    adding new custom directive

---
+++ src/app/class.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appClass]',
+})
+export class ClassDirective {
+  @Input() backgroundColor: string;
+
+  constructor(private element: ElementRef) {
+    setTimeout(() => {
+      this.element.nativeElement.style.backgroundColor = this.backgroundColor;
+    }, 50);
+  }
+}
